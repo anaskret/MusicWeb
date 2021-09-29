080c58b79277151ea568765faefb75ab5c67477e
Monika Nogała <mxnogala@gmail.com>
2021-09-29 22:02:19 +0200

    Header rebuild (rating, info and bio)

--- MusicWeb.Client/src/components/Header.vue
+++ MusicWeb.Client/src/components/Header.vue
@@ -1,31 +1,17 @@
 <template>
-  <v-container fluid>
+  <v-container fluid class="py-16">
     <v-row justify="center">
-      <v-col class="mt-6" lg="3" sm="6">
+      <v-col md="3" sm="6">
         <div>
-          <v-img
-            :src="require('@/assets/BandPhoto.svg')"
-            contain
-            width="402"
-            height="395"
-          />
+          <v-img :src="require('@/assets/BandPhoto.svg')" contain />
         </div>
       </v-col>
-      <!-- Główna kolumna -->
-      <v-col class="mt-9" lg="6" sm="12">
-        <v-row>
-          <!-- Kolumna z tytułem -->
+      <v-col md="7" sm="12">
+        <v-row class="d-flex align-items-center">
           <v-col lg="12" sm="7">
-            <div class="d-flex align-items-center">
+            <div class="d-flex">
               <div>
-                <h1
-                  class="
-                    display-3
-                    font-weight-bold
-                    text-left text-uppercase
-                    artist-title
-                  "
-                >
+                <h1 class="text-uppercase display-3 font-weight-bold">
                   ANATHEMA
                 </h1>
               </div>
@@ -33,82 +19,78 @@
                 outlined
                 color="grey"
                 height="30px"
-                class="text-uppercase ml-16"
-                >Obserwuj</v-btn
-              >
+                class="text-uppercase align-self-center ml-16"
+                >Obserwuj
+              </v-btn>
             </div>
           </v-col>
-          <!-- nowy row -->
-          <div>
-            <p class="mb-8 text-left">Dodaj do ulubionych:</p>
-            <p class="mb-8 text-left">
-              <font-awesome-icon
-                class="icon"
-                icon="heart"
-                size="2x"
-                color="#865e61"
-              />
-              <span>199 osób</span> dodało do ulubionych
-            </p>
-          </div>
-          <v-col class="mt-14" lg="4" sm="12">
-            <v-card align="flex-start">
-              <p class="mb-8">Oceń artystę:</p>
-              <p class="mb-8">
+        </v-row>
+        <v-row>
+          <v-col md="12" class="rating">
+            <!-- TODO Style => bootstrap class -->
+            <div>
+              <p>Dodaj do ulubionych</p>
+              <p>
+                <font-awesome-icon
+                  class="icon"
+                  icon="heart"
+                  size="2x"
+                  color="#865e61"
+                ></font-awesome-icon>
+                <span>199 osób</span> dodało do ulubionych
+              </p>
+            </div>
+            <div>
+              <p>Oceń artystę</p>
+              <p>
                 <font-awesome-icon
                   class="icon"
                   v-for="(star, index) in stars"
                   :key="index"
                   icon="star"
                   size="2x"
-                  :color="star.color"
-                />
-                <span> 4.0</span>
+                  :colot="star.color"
+                ></font-awesome-icon>
+                <span>4.0</span>
               </p>
-            </v-card>
-          </v-col>
-          <v-col class="my-9" lg="12" sm="8">
-            <v-card class="my-9">
-              <h1 class="display-1 font-weight-bold mb-12 text-left">
-                Biografia
-              </h1>
-              <p class="text-justify">
-                {{ artist.bio }}
-              </p>
-            </v-card>
+            </div>
           </v-col>
         </v-row>
       </v-col>
     </v-row>
+    <v-row justify="center">
+      <v-col md="3" sm="6">
+        <h1 class="title font-weight-bold">Informacje</h1>
+        <p>Rok założenia: <span>1990</span></p>
+        <p>Pochodzenie: <span>Liverpool, Anglia</span></p>
+        <p>
+          Gatunek muzyczny: <span>rock progresywny, art rock, doom metal</span>
+        </p>
+        <p>
+          Członkowie:
+          <span
+            >Vincent Cavanagh, Daniel Cavanagh, Jaime Cavanagh, Lee Douglas,
+            John Douglas</span
+          >
+        </p>
+      </v-col>
+      <v-col lg="7">
+        <h1>Biografia</h1>
+        <p class="text-justify">
+          Anathema (gr. klątwa) to angielska grupa muzyczna. Powstała 1990 roku
+          w Liverpoolu. Początkowo wykonująca drugofalowy death metal/doom
+          metal, będąc wraz z grupami Katatonia, Tiamat, Winter, My Dying Bride
+          i Paradise Lost prekursorem gatunku. Obecnie grupa zaliczana jest do
+          nurtu art rocka oraz rocka atmosferycznego. Anathema została stworzona
+          w 1990 roku pod nazwą Pagan Angel. W listopadzie tego roku zespół
+          nagrał pierwsze demo "An Iliad of Woes". Zmiana nazwy nastąpiła na
+          początku 1991 roku. Pierwsze demo przyciągnęło uwagę zespołów z
+          brytyjskiej sceny metalowej i pozwoliło Anathemie zagrać koncerty u
+          boku takich kapel jak Bolt Thrower czy Paradise Lost.
+        </p>
+      </v-col>
+    </v-row>
   </v-container>
-  <!-- <v-card class="my-9 text-left border-right-4 border-danger">
-          <h1 class="title font-weight-bold mb-12">Informacje</h1>
-          <p>
-            Rok założenia:
-            <span>{{
-              artist.establishmentDate /* Convert ot real date */
-            }}</span>
-          </p>
-          <p>
-            Pochodzenie:
-            <span
-              >{{ address.city }} {{ address.state }},
-              {{ address.country }}</span
-            >
-          </p>
-          <p>
-            Gatunek muzyczny:
-            <span>rock progresywny, art rock, doom metal</span>
-          </p>
-          <p v-if="artist.isBand && !artist.isIndividual">
-            Członkowie:
-            <span
-              >Vincent Cavanagh, Daniel Cavanagh, Jaime Cavanagh, Lee Douglas,
-              John Douglas</span
-            >
-          </p>
-        </v-card>
-     -->
 </template>
 
 <script>
@@ -136,28 +118,14 @@ export default {
     };
   },
   mounted() {
-    debugger;
     console.log(this.address);
   },
 };
 </script>
 
 <style scoped>
-/* .artist-title {
-  letter-spacing: 5px !important;
+.rating {
+  display: flex;
+  justify-content: space-between;
 }
-p {
-  color: gray;
-}
-p > span {
-  color: #ebebf2;
-  padding-bottom: 1px;
-}
-v-card {
-  box-shadow: 0px red;
-}
-.icon {
-  position: relative;
-  top: 5px;
-} */
 </style>
