7479b2aa4fa27a77a002335209d6ce4325f2f6e7
anaskret <aleksandernaskret@gmail.com>
2021-10-11 20:33:05 +0200

    User Update

--- MusicWeb.Server/MusicWeb.Models/Models/Identity/RegisterModel.cs
+++ MusicWeb.Server/MusicWeb.Models/Models/Identity/RegisterModel.cs
@@ -11,7 +11,22 @@ namespace MusicWeb.Models.Models.Identity
     {
         [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
+
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+        public DateTime BirthDate { get; set; } = DateTime.Now;
     }
 }
