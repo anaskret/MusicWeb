1d084552cabf9dc3f83d1673283b307740706c1c
anaskret <aleksandernaskret@gmail.com>
2021-11-11 19:35:14 +0100

    ChatController

---
+++ MusicWeb.Server/MusicWeb.Api/Controllers/Chats/ChatController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicWeb.Api.Controllers.Chats
+{
+    [ApiController]
+    public class ChatController : Controller
+    {
+        private readonly IChatService _chatService;
+
+        public ChatController(IChatService chatService)
+        {
+            _chatService = chatService;
+        }
+    }
+}
---
+++ MusicWeb.Server/MusicWeb.Services/Interfaces/Chats/IChatService.cs
@@ -0,0 +1,17 @@
+﻿using MusicWeb.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicWeb.Services.Interfaces.Chats
+{
+    public interface IChatService
+    {
+        Task<List<Chat>> GetChatsByUserId(string userId);
+        Task AddChat(Chat entity);
+        Task UpdateChat(Chat entity);
+        Task DeleteChat(int id);
+    }
+}
