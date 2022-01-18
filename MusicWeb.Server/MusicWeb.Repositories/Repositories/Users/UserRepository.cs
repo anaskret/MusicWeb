20e5aa18d188717bdad356b959f3cb08aa2f0bc0
Monika Nogała <mxnogala@gmail.com>
2022-01-18 21:18:28 +0100

    User observed fix

--- MusicWeb.Server/MusicWeb.Repositories/Repositories/Users/UserRepository.cs
+++ MusicWeb.Server/MusicWeb.Repositories/Repositories/Users/UserRepository.cs
@@ -25,6 +25,8 @@ namespace MusicWeb.Repositories.Repositories.Users
         public async Task<ApplicationUser> GetUserByIdAsync(string id)
         {
             var user = await _dbContext.Users.Include(prp => prp.UserFavoriteArtists)
+                                              .ThenInclude(prp => prp.Artist)
+                                             .Include(prp => prp.UserObservedArtists)
                                              .ThenInclude(prp => prp.Artist)
                                              .Include(prp => prp.UserFavoriteAlbums)
                                              .ThenInclude(prp => prp.Album)
@@ -32,7 +34,6 @@ namespace MusicWeb.Repositories.Repositories.Users
                                              .ThenInclude(prp => prp.Song)
                                              .Include(prp => prp.UserFriends)
                                              .ThenInclude(prp => prp.Friend)
-                                             .Include(prp => prp.UserObservedArtists)
                                              .Include(prp => prp.AlbumReviews)
                                              .Include(prp => prp.SongReviews)
                                              .FirstOrDefaultAsync(prp => prp.Id == id);
